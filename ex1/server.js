979ed66b07f40f4256ba3271a46c48096942fa29
Santhana Pandi <198602096+santhanam494@users.noreply.github.com>
2026-02-05 18:43:16 +0530

    Change server port to use environment variable

--- ex1/server.js
+++ ex1/server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
+
 
 // Middleware
 app.use(cors());
